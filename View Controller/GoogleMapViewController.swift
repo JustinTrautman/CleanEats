25f1709404d4f7ba5c89bbc4b3722ab179f2fe89
Justin Trautman <justin@modularmobile.net>
2018-06-29 11:50:25 -0600

    take out landing screen and put up dummy launchScreen

---
+++ View Controller/GoogleMapViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GoogleMapViewController.swift
+//  CleanEats
+//
+//  Created by Justin Trautman on 6/28/18.
+//  Copyright © 2018 Justin Trautman. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class GoogleMapViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var findFoodButton: UIButton!
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       setupMapView()
+    }
+    
+    func setupMapView() {
+        
+        GMSServices.provideAPIKey("AIzaSyBHh-f0KqLhwy828wB7HUYLOt81w4FhmZw")
+        GMSPlacesClient.provideAPIKey("AIzaSyBHh-f0KqLhwy828wB7HUYLOt81w4FhmZw")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 47.6588, longitude: -117.4260, zoom: 10)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 47.6588, longitude: -117.4260)
+        marker.title = "Spokane"
+        marker.snippet = "Washington"
+        marker.map = mapView
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Views/RestaurantTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RestaurantTableViewCell.swift
+//  CleanEats
+//
+//  Created by Justin Trautman on 6/28/18.
+//  Copyright © 2018 Justin Trautman. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
