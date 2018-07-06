4417e0590d44195dd5bb2231bbdfeea18b70b8fd
Justin Trautman <justin@modularmobile.net>
2018-07-06 17:20:36 -0600

    Changes Appdelegate

--- CleanEats/AppDelegate.swift
+++ CleanEats/AppDelegate.swift
@@ -13,9 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        print("Hello, world!")
         return true
     }
 
