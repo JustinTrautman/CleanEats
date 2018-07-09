71fca6f148240ca76212cb195cfce6aa7230b944
huzaifa83 <huzaifagadiwala@gmail.com>
2018-07-09 15:08:59 -0600

    Adds favorated array

--- CleanEats/View Controller/FavoriteTableViewController.swift
+++ CleanEats/View Controller/FavoriteTableViewController.swift
@@ -33,20 +33,20 @@ class FavoriteTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return RestaurantController.sharedRestaurant.favoriteRestaurants.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "favoriteRestaurant", for: indexPath)
-
-        // Configure the cell...
-
+        let restaurant = RestaurantController.sharedRestaurant.favoriteRestaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.restaurantTitle
+        
         return cell
     }
  
@@ -58,9 +58,7 @@ class FavoriteTableViewController: UITableViewController {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }  
     }
  
 
