9972ba08cecba47d5e21ae0f76afc09656f4fdf7
huzaifa83 <huzaifagadiwala@gmail.com>
2018-07-09 14:26:39 -0600

    Updated project

--- CleanEats/Model Controller/RestaurantController.swift
+++ CleanEats/Model Controller/RestaurantController.swift
@@ -13,12 +13,9 @@ class RestaurantController {
     // Source of truth
     var restaurants: [Restaurant] {
         let restuarunts = [
-        let Cubbys = Restaurant(restaurantImage: #imageLiteral(resourceName: "Burger"), restaurantTitle: "Cubbys", restaurantRating: #imageLiteral(resourceName: "5Stars"), ratingCount: 3, restaurantDistance: "1.8 Miles Away", restaurantDescription: "America(New), Sandwiches, Salad", restaurantPrice: "$$", restaurantRisk: "18", ),
-        
-        let fuddruckers = Restaurant(restaurantImage: #imageLiteral(resourceName: "Fuddruckers"), restaurantTitle: "Fuddruckers", restaurantRating: #imageLiteral(resourceName: "5Stars"), ratingCount: 4, restaurantDistance: "3 Miles Away", restaurantDescription: "Make your own sandwhiches", restaurantPrice: "$", restaurantRisk: "20"),
-        
-        let apolloBurger = Restaurant(restaurantImage: #imageLiteral(resourceName: "Apollo Burger"), restaurantTitle: "Apollo Burger", restaurantRating: #imageLiteral(resourceName: "5Stars"), ratingCount: 5, restaurantDistance: "1.5 Miles Away", restaurantDescription: "Burgers, fries", restaurantPrice: "$", restaurantRisk: "15"),
-
+            Restaurant(restaurantImage: #imageLiteral(resourceName: "Burger"), restaurantTitle: "Cubbys", restaurantRating: #imageLiteral(resourceName: "5Stars"), ratingCount: 3, isFavorite: true, restaurantDistance: "1.8 Miles Away", restaurantDescription: "America(New), Sandwiches, Salad", restaurantPrice: "$$", restaurantRisk: "18"),
+            Restaurant(restaurantImage: #imageLiteral(resourceName: "Fuddruckers"), restaurantTitle: "Fuddruckers", restaurantRating: #imageLiteral(resourceName: "5Stars"), ratingCount: 4, isFavorite: true, restaurantDistance: "3 Miles Away", restaurantDescription: "Make your own sandwhiches", restaurantPrice: "$", restaurantRisk: "20"),
+            Restaurant(restaurantImage: #imageLiteral(resourceName: "Apollo Burger"), restaurantTitle: "Apollo Burger", restaurantRating: #imageLiteral(resourceName: "5Stars"), ratingCount: 5, isFavorite: true, restaurantDistance: "1.5 Miles Away", restaurantDescription: "Burgers, fries", restaurantPrice: "$", restaurantRisk: "15")
         ]
         
         return restuarunts
