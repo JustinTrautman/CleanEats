207cc1a940c31760a96d4a7c26f5fbd7d6994591
JoshuaDanner <jdanner3@gmail.com>
2018-07-09 23:07:21 -0600

    Got views working

---
+++ CleanEats/Views/HealthRatingTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HealthRatingTableViewCell.swift
+//  CleanEats
+//
+//  Created by Joshua Danner on 7/9/18.
+//  Copyright © 2018 Justin Trautman. All rights reserved.
+//
+
+import UIKit
+
+class HealthRatingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func inspectionInfoButton(_ sender: UIButton) {
+    }
+}
