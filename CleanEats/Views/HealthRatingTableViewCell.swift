abf690fe6c9567890c7b26f7a61aeb8c6f4e3e42
JoshuaDanner <jdanner3@gmail.com>
2018-07-10 21:31:56 -0600

    Added violationsView

--- CleanEats/Views/HealthRatingTableViewCell.swift
+++ CleanEats/Views/HealthRatingTableViewCell.swift
@@ -8,34 +8,58 @@
 
 import UIKit
 
+protocol healthRatingTableViewCellDelegate: class {
+    func infoButtonTapped(cell: HealthRatingTableViewCell)
+}
+
 class HealthRatingTableViewCell: UITableViewCell {
     
+    weak var cellDelegate: healthRatingTableViewCellDelegate?
     // MARK: IBOutlets
     
  
     @IBOutlet weak var infoButton: UIButton!
     
+    @IBOutlet weak var violationView: UIView!
+    @IBOutlet weak var violationViewSubview: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
+        initializeViolationView()
         // Configure the view for the selected state
+    }
+    
+    func initializeViolationView() {
+        // Find Food Button
+        violationView.layer.cornerRadius = 3.0
+        violationView.clipsToBounds = false
+        
+        violationView.layer.shadowRadius = 7.0
+        violationView.layer.shadowColor = UIColor.black.cgColor
+        violationView.layer.shadowOpacity = 0.4
+        violationView.layer.shadowOffset = CGSize.zero
+        violationView.layer.masksToBounds = false
+        violationView.layer.shouldRasterize = true
+        violationViewSubview.layer.masksToBounds = false
+        violationViewSubview.layer.cornerRadius = 3
+
     }
     
     // MARK: IBActions
     
-    @IBAction func infoButtonTapped(_ sender: UIButton) {
+
+    @IBAction func infoButtonTapped(_ sender: Any) {
+        print("infoButtonTapped")
+        cellDelegate?.infoButtonTapped(cell: self)
+        let violationsAlert = UIAlertController(title: "Critical Violation", message: "This is an alert.", preferredStyle: .alert)
+        violationsAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         
-        let alert = UIAlertController(title: "Criticl Violation", message: "This is an alert.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-            NSLog("The \"OK\" alert occured.")
-        }))
-        alert.present(alert, animated: true, completion: nil)
+        //present(violationsAlert, animated: true, completion: nil)
     }
 }
     
