782a651876441dcf504d681d59c7fff9fefc813d
Justin Trautman <justin@modularmobile.net>
2018-07-17 08:16:29 -0600

    Removes unneeded star assets

---
+++ CleanEats/Model Controller/HealthDataController.swift
@@ -0,0 +1,14 @@
+//
+//  HealthDataController.swift
+//  CleanEats
+//
+//  Created by Justin Trautman on 7/14/18.
+//  Copyright © 2018 Justin Trautman. All rights reserved.
+//
+
+import Foundation
+
+class HealthDataController {
+    
+    
+}
